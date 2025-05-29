116c010e595a66cdd024f45e1c5411cfdb7b38c9
Dell <putraiman297@gmail.com>
2025-05-29 21:52:38 +0800

    first commit9

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const PORT = 3001;
+const PORT = 3002;
 
 // Middleware
 app.use(cors()); // Izinkan semua origin untuk pengembangan, bisa diperketat di produksi
